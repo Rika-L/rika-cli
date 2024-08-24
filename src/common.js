0f3bfc21b74ece445f95c68395028b2c08c9e0b6
Rika-L <2209393334@qq.com>
2024-08-24 22:53:44 +0800

    fix: could not move file

--- src/common.js
+++ src/common.js
@@ -2,7 +2,7 @@ import {DOWNLOAD_DIRECTORY, GIT_URL} from "./constant.js";
 import axios from "axios";
 import ora from "ora";
 import {gitClonePromise} from '@rika_/git-clone'
-import {rimraf} from 'rimraf'
+import {rimraf, rimrafSync} from 'rimraf'
 import path from "node:path";
 import ncp from "ncp";
 
@@ -36,7 +36,7 @@ async function downDir(repo) {
 async function copyTempToTarget(repo, projectName) {
     let dest = `${DOWNLOAD_DIRECTORY}/${repo}`;
     const resolvePath = path.join(path.resolve(), projectName);
-    await rimraf(resolvePath)
+    rimrafSync(resolvePath)
     await ncp(dest, resolvePath);
 }
 
